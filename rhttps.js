f14cc216bb52a7ade8401a3fc9de90d6a59b978d
admin <admin@M135670.jrcad.jrc.co.jp>
2019-04-12 16:16:29 +0900

    first commit

---
+++ rhttps.js
@@ -0,0 +1,243 @@
+var PORT = 10443;
+var SSL_KEY = 'server.key';
+var SSL_CERT = 'server.crt';
+
+var keepAliveStart = false;
+var ttlVal = 3;
+var keepAliveTime = (new Date()).getTime();
+
+var path = require('path');
+var fs = require("fs");
+var url = require("url");
+
+var mime = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript"
+};
+
+var options = {
+    key: fs.readFileSync(SSL_KEY).toString(),
+    cert: fs.readFileSync(SSL_CERT).toString()
+};
+
+const getUniqueId = getUniqueIdMaker();
+
+// サーバの初期化
+var server = require("https").createServer(options, function (req, res) {
+    var urlParse = url.parse(req.url, true);
+
+    var filePath;
+    if (urlParse.pathname == '/') {
+        filePath = '/index.html';
+    } else {
+        filePath = urlParse.pathname;
+    }
+    //console.log("req.url=" + req.url);
+    //console.log("filePath=" + filePath);
+    //console.log(JSON.stringify(urlParse));
+
+    var fullPath = __dirname + filePath;
+    fs.readFile(fullPath, function (err, data) {
+        if (err) {
+            console.log("NO FILE: " + filePath);
+            res.writeHead(500);
+            res.end('Error loading ' + filePath);
+        } else {
+            res.writeHead(200, { "Content-Type": mime[path.extname(fullPath)] || "text/html" });
+            if (urlParse.pathname == "/index.html" && urlParse.query.user_id) {
+                data = data.toString().replace("initial_user_id", urlParse.query.user_id);
+            }
+            res.end(data);
+        }
+    });
+}).listen(process.env.PORT || PORT);
+var io = require("socket.io").listen(server);
+
+// ユーザ管理ハッシュ
+var userHash = {};
+var user_sid = {};
+//-------------------------------------------------------------
+// userHash = {
+//   id : { lat : lat,  lng : lng, ttl : ttl}
+// }
+//-------------------------------------------------------------
+// user_sid = {
+//   id : socket_id,
+// }
+//-------------------------------------------------------------
+
+// イベントの定義
+io.on("connection", function (socket) {
+
+    // 接続開始
+    socket.on("connected", function (_id) {
+        console.log('ENTERED:' + _id);
+        user_sid[_id] = socket.id;
+        socket.broadcast.emit("start", { id: _id });
+        console.log("CONNECTED: USER_ID=" + _id + " USER_SID=" + JSON.stringify(user_sid));
+    });
+
+    // 登録
+    socket.on("regist", function (_id) {
+        console.log('ON REGIST:' + _id);
+        user_sid[_id] = socket.id;
+        socket.broadcast.emit("regist", JSON.stringify({ id: _id }));
+    });
+
+    // // 登録応答
+    // socket.on("regist-apply", function (msg) {
+    //     var data = JSON.parse(msg);
+    //     console.log(`ON REGIST-APPLY: DEST=${data.dest} SRC=${data.src}`);
+    //     if (!user_sid[data.src]){
+    //         user_sid[data.src] = socket.id;
+    //     }
+    //     socket.to(user_sid[data.dest]).emit("regist-apply", msg);
+    // });
+
+    // // Senderからの映像ソース変更
+    // socket.on("video_init", function (msg) {
+    //     var data = JSON.parse(msg);
+    //     console.log(`ON VIDEO_INIT: DEST=${data.dest} SRC=${data.src}`);
+    //     socket.to(user_sid[data.dest]).emit("video_init", msg);
+    // });
+
+    // P2P開始
+    socket.on("start", function (msg) {
+        var data = JSON.parse(msg);
+        console.log(`ON START: ${data.src} -> ${data.dest}  MSG=${msg}`);
+        if (data.dest) {
+            socket.to(user_sid[data.dest]).emit("start", JSON.stringify({ id: data.src }));
+        } else {
+            socket.broadcast.emit("start", JSON.stringify({ id: data.src }));
+        }
+    });
+
+    // メッセージ送信
+    socket.on("publish", function (msg) {
+        console.log(`publish=${msg}`)
+        var data = JSON.parse(msg);
+        if (data.dest) {
+            //socket.broadcast.emit("publish", msg);
+            console.log(`data.dest=${data.dest}`);
+            console.log(`user_sid[data.dest] =${user_sid[data.dest]}`);
+            socket.to(user_sid[data.dest]).emit("publish", msg);
+        } else {
+            socket.broadcast.emit("publish", msg);
+        }
+        console.log(`PUBLISH MSG: ${msg}`);
+    });
+
+    // 位置情報着信
+    socket.on("renew", function (msg) {
+        var data = JSON.parse(msg);
+        user_sid[data.id] = socket.id;
+
+        //console.log(`ON RENEW : From=${data.id} LAT=${data.lat} LNG=${data.lng} CAM=${data.cam}`);
+
+        if (data.id) {
+            userHash[data.id] = { lat: data.lat, lng: data.lng, ttl: ttlVal, cam: data.cam };
+            //console.log("RECIVE:USERHASH=" + JSON.stringify(userHash));
+        }
+    });
+
+    // カメラ情報設定
+    socket.on("camera", function (msg) {
+        var data = JSON.parse(msg);
+        console.log(`ON CAMERA : ID=${data.id} CAM=${data.cam}`);
+
+        if (data.id) {
+            if (userHash[data.id]) {
+                userHash[data.id].cam = data.cam;
+                //userHash[data.id].cam = true;
+            } else {
+                userHash[data.id] = { lat: null, lng: null, ttl: ttlVal, cam: data.cam };
+                //userHash[data.id] = { lat: null, lng: null, ttl: ttlVal, cam: true };
+            }
+        }
+    });
+
+    // 切断
+    socket.on("disconnect", function (reason) {
+        console.log(`DISCONNECT msg=${reason}`);
+        //console.log(`socket.id=${socket.id}`);
+        //if (reason.indexOf("transport error") != -1) {
+        // Object.keys(user_sid).forEach(function(_id){
+        //     if (user_sid[_id] == socket.id){
+        //         delete userHash[_id];
+        //         delete user_sid[_id];
+        //         io.sockets.emit("user_disconnect", JSON.stringify({ id: _id}));
+        //     }
+        // });
+    });
+
+    // 接続終了(接続元ユーザを削除し、他ユーザへ通知)
+    /*
+    Object.keys(user_sid).forEach(function(_id){
+        if (socket.id == user_sid[_id]) {
+            delete userHash[_id];
+            delete user_sid[_id];
+            var msg = {id: _id};
+            socket.broadcast.emit("disconect", JSON.stringify(msg));
+        }
+    });
+    console.log("DELETE:USERHASH=" + JSON.stringify(userHash));        
+    //socket.broadcast.emit("renew", { value: JSON.stringify(userHash) });   
+    */
+
+    setInterval(function () {
+        socket.emit("renew", JSON.stringify(userHash));
+        //console.log(`SEND RENEW : USERHASH=${JSON.stringify(userHash)}`);
+        //console.log(`SEND RENEW : USER_SID=${JSON.stringify(user_sid)}`);
+        //console.log("TIME: " + (new Date()).getTime());
+
+        //----------------------------------------------------------
+        // Keep Alive (CHECK TTL)
+        //----------------------------------------------------------
+        const now = (new Date()).getTime();
+        if (now >= keepAliveTime + 1500) {
+            keepAliveTime = now;
+            Object.keys(userHash).forEach(function (id) {
+                userHash[id].ttl = userHash[id].ttl - 1;
+                if (userHash[id].ttl < 0) {
+                    delete userHash[id];
+                    //delete user_sid[id];
+                    console.log(`DELETE id=${id} USER_HASH=${JSON.stringify(userHash)}`);
+                }
+            });
+        }
+        //console.log("USERHASH=" + JSON.stringify(userHash));
+        //console.log("USER_SID=" + JSON.stringify(user_sid));
+    }, 1500);
+});
+
+
+
+
+// 番号をアルファベットに変換（27進数）
+function getAlphabet(no) {
+    return getAlphabetExec(no + 1);
+}
+function getAlphabetExec(no) {
+    if (no == 0) {
+        return "";
+    }
+    else if (no < 27) {
+        return String.fromCharCode(0x40 + no);
+    }
+    else {
+        let upper = Math.floor(no / 27);
+        let lower = no % 27;
+        return getAlphabetExec(upper) + getAlphabetExec(lower);
+    }
+}
+
+// ユニークID
+function getUniqueIdMaker() {
+    let userId = 0;
+    return function () {
+        let alpha = getAlphabet(userId);
+        userId++;
+        return alpha;
+    }
+}
---
+++ rtc.js
@@ -0,0 +1,206 @@
+const socketio = io.connect();
+
+const $local_id = document.getElementById("local_id");
+const $local_name = document.getElementById("local_name");
+const $local_elm = document.getElementById("local_elm");
+const $remote = document.getElementById("remote");
+const remotes = {};
+let local_id = null;
+let local_stream = null;
+
+constraints = {
+    audio: true,
+    video: {
+        width: {
+            min: 320,
+            max: 640
+        },
+        height: {
+            min: 240,
+            max: 480
+        },
+        frameRate: 20,
+        //facingMode: { exact: 'environment' }
+        //facingMode: { exact: 'user' }
+    }
+}
+
+//constraints = { video: true, audio: true }
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (stream) {
+        local_stream = stream;
+        $local_elm.srcObject = local_stream;
+        $local_elm.onloadedmetadata = function (e) {
+            $local_elm.play();
+        };
+        $local_elm.play();
+    })
+    .catch(function (err) {
+        console.log(`gUM error:${err}`);
+    });
+
+$local_id.onchange = function (ev) {
+    console.log(`$local_id:${ev.target.value}`);
+    local_id = ev.target.value;
+    $local_id.style.display = "none";
+    $local_name.innerText = local_id;
+    $local_name.style.display = "block";
+    setInterval(function () {
+        socketio.emit("renew", JSON.stringify({ id: local_id }));
+    }, 1500);
+}
+
+const Create_elm = function (name, parent, a_class) {
+    this.$li = document.createElement("li");
+    this.$name = document.createElement("span");
+    this.$name.classList.add("text");
+    this.$name.innerText = name;
+    this.$media = document.createElement("video");
+    this.$media.classList.add("video");
+    this.$media.style.display = "none";
+    this.$media.setAttribute("playsinline", true);
+    this.$li.appendChild(this.$name);
+    this.$li.appendChild(this.$media);
+    this.$li.classList.add(a_class);
+    parent.appendChild(this.$li);
+    this.count = 0;
+}
+Create_elm.prototype.get_elm = function () {
+    return this.$li;
+}
+Create_elm.prototype.show = function (ev) {
+    if (ev.track.kind == "video") {
+        this.$media.srcObject = ev.streams[0];
+        this.$media.style.display = "block";
+        this.$media.play();
+    }
+}
+Create_elm.prototype.delete = function () {
+    this.$li.removeChild(this.$media);
+    this.$li.removeChild(this.$name);
+    this.$li.parentNode.removeChild(this.$li);
+}
+Create_elm.prototype.on = function (event, handler) {
+    this.$li.addEventListener(event, handler);
+}
+
+socketio.on("renew", function (msg) {
+    //console.log(`renew=${msg}`)
+    const data = JSON.parse(msg);
+
+    if (!local_id) return;
+    if (!local_stream) return;
+
+    const cur_users = Object.keys(remotes)
+
+    Object.keys(data).forEach(function (new_user) {
+        if (!cur_users.includes(new_user) && new_user != local_id) {
+            remotes[new_user] = {};
+            remotes[new_user].obj = new Create_elm(new_user, $remote, "user_list");
+            remotes[new_user].elm = remotes[new_user].obj.get_elm();
+            remotes[new_user].peer = new RTCPeerConnection({
+                //sdpSemantics : "unified-plan",
+                sdpSemantics: "plan-b",
+                iceServers: [
+                    { urls: "stun:stun.stunprotocol.org" },
+                    { urls: 'stun:stun.l.google.com:19302' },
+                    { urls: 'stun:23.21.150.121' }
+                ]
+            });
+
+            remotes[new_user].peer.onicecandidate = function (ev) {
+                console.log(`onicecandidate:ev=${JSON.stringify(ev)}`);
+
+                if (ev.candidate) {
+                    socketio.emit("publish", JSON.stringify(
+                        {
+                            type: "candidate",
+                            dest: new_user,
+                            src: local_id,
+                            candidate: ev.candidate
+                        })
+                    )
+                }
+            }
+            remotes[new_user].peer.ontrack = function (ev) {
+                console.log(`ontrack ev=${JSON.stringify(ev)}`);
+
+                if (ev.streams && ev.streams[0]) {
+                    remotes[new_user].obj.show(ev);
+                }
+            }
+
+            remotes[new_user].video_sender = remotes[new_user].peer.addTrack(local_stream.getVideoTracks()[0], local_stream);
+            remotes[new_user].audio_sender = remotes[new_user].peer.addTrack(local_stream.getAudioTracks()[0], local_stream);
+
+            remotes[new_user].obj.on("click", function () {
+                remotes[new_user].peer.createOffer()
+                    .then(function (offer) {
+                        console.log(`setLocalDescription`);
+                        const local_sdp = new RTCSessionDescription(offer);
+                        return remotes[new_user].peer.setLocalDescription(local_sdp);
+                    })
+                    .then(function () {
+                        console.log(`offer emit to=${new_user}`);
+
+                        socketio.emit("publish", JSON.stringify(
+                            {
+                                type: "offer",
+                                dest: new_user,
+                                src: local_id,
+                                sdp: remotes[new_user].peer.localDescription
+                            })
+                        );
+                    })
+            })
+        }
+    });
+
+    cur_users.forEach(function (cur_user) {
+        if (!Object.keys(data).includes(cur_user)) {
+            console.log(`delete ${cur_user}`);
+            remotes[cur_user].obj.delete();
+            delete remotes[cur_user].peer;
+            delete remotes[cur_user];
+        }
+    })
+});
+
+socketio.on("publish", function (msg) {
+    const data = JSON.parse(msg);
+
+    if (data.dest == local_id) {
+        if (data.type == "offer") {
+            const remote_sdp = new RTCSessionDescription(data.sdp);
+            remotes[data.src].peer.setRemoteDescription(remote_sdp)
+                .then(function () {
+                    console.log(`createAnswer`);
+                    return remotes[data.src].peer.createAnswer();
+                })
+                .then(function (answer) {
+                    console.log(`setLocalDescription answer`);
+                    const local_sdp = new RTCSessionDescription(answer);
+                    remotes[data.src].peer.setLocalDescription(local_sdp);
+                    socketio.emit("publish", JSON.stringify(
+                        {
+                            type: "answer",
+                            dest: data.src,
+                            src: local_id,
+                            sdp: remotes[data.src].peer.localDescription
+                        })
+                    );
+                })
+                .catch(function (err) {
+                    console.log(`signal: ${err}`)
+                });
+        } else if (data.type == "answer") {
+            console.log(`setRemoteDescription`);
+            const remote_sdp = new RTCSessionDescription(data.sdp);
+            remotes[data.src].peer.setRemoteDescription(remote_sdp)
+        } else if (data.type == "candidate") {
+            console.log(`addIceCandidate`);
+            const candidate = new RTCIceCandidate(data.candidate);
+            remotes[data.src].peer.addIceCandidate(candidate);
+        }
+    }
+})
